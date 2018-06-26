47545245f165aefe043713bd37e0fcc60238f26c
Rafael Wolf de Goes <rewgoes@gmail.com>
2018-06-25 22:13:33 -0300

    feat: add tabs navigation
    react-navigation {
        createBottomTabNavigator
        createMaterialTopTabNavigator
    }

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { View, StatusBar } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
+import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
-import { purple } from './utils/colors'
+import { createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
@@ -15,13 +18,43 @@ function UdaciStatusBar({ backgroundColor, ...props }) {
   )
 }
 
+const routes = {
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    },
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    },
+  },
+};
+
+const tabOptions = {
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+    }
+  }
+}
+
+const Tabs = Platform.OS === "ios"
+  ? createBottomTabNavigator(routes, tabOptions)
+  : createMaterialTopTabNavigator(routes, tabOptions);
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
           <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-          <History />
+          <Tabs />
         </View>
       </Provider>
     )
